5fb455e4ae6d9e621ec43ba4aa75089e20745ca1
bonesy-maetrix <jordan.schalter@maetrix.online>
2025-03-31 14:36:22 -0400

    Add files via upload
    right

--- app.py
+++ app.py
@@ -50,3 +50,7 @@ def download_profiles():
 def get_zip():
     path = request.args.get("path")
     return send_file(path, as_attachment=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
